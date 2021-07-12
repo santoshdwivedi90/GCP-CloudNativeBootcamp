bc8bc1c18a5c154600c6f108b9999248212f382b
sadwi3 <santoshkumar.dwivedi@ingka.ikea.com>
2021-07-12 15:42:47 +0200

    cloudbuild file

--- src/main/java/functions/HelloWorld.java
+++ src/main/java/functions/HelloWorld.java
@@ -40,6 +40,6 @@ public class HelloWorld implements HttpFunction {
     }
 
     var writer = new PrintWriter(response.getWriter());
-    writer.printf("Hello hjiSirji%s!", name);
+    writer.printf("Hello %s!", name);
   }
 }
