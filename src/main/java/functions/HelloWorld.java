4787a8536315843357ec31933884488cc9c76b7d
sadwi3 <santoshkumar.dwivedi@ingka.ikea.com>
2021-07-21 15:45:21 +0200

    initial commits

--- src/main/java/functions/HelloWorld.java
+++ src/main/java/functions/HelloWorld.java
@@ -40,6 +40,6 @@ public class HelloWorld implements HttpFunction {
     }
 
     var writer = new PrintWriter(response.getWriter());
-    writer.printf("Hello ji %s!", name);
+    writer.printf("Hello i %s!", name);
   }
 }
