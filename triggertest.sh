ad7838c9af333b831233fd80a09451d1f02f9dc9
sadwi3 <santoshkumar.dwivedi@ingka.ikea.com>
2021-07-21 17:34:27 +0200

    initial commits

--- triggertest.sh
+++ triggertest.sh
@@ -1,3 +1,3 @@
 
 npm install -g newman
-newman run -d example-csv-all-types.csv -r htmlextra.cli CreateUSers.postman_collection.json
+newman run -d example-csv-all-types.csv  CreateUSers.postman_collection.json
